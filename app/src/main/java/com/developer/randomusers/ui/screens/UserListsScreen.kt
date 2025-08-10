adc3c030f31b714d317dcae929888649d5434387
NonAdminWork <nonadminwork@MacBookPro.attlocal.net>
2025-08-09 22:16:16 -0500

    adding new line

--- app/src/main/java/com/developer/randomusers/ui/screens/UserListsScreen.kt
+++ app/src/main/java/com/developer/randomusers/ui/screens/UserListsScreen.kt
@@ -67,6 +67,7 @@ fun UserListScreen(
 
     val debouncedSearchText by viewModel.debouncedUserInputTextForSearch.collectAsStateWithLifecycle()
     val searchText by viewModel.userInputTextForSearch.collectAsStateWithLifecycle()
+    
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
