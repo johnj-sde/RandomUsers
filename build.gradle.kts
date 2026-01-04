6ed2f06235e1fd9e83e248a4a313d48b6208dc33
NonAdminWork <nonadminwork@Johns-MacBook-Pro.local>
2026-01-03 21:46:32 -0600

    updating to run automated instrumentation tests during deployment

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,5 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.jetbrains.kotlin.serialization) apply false
+    id("com.google.gms.google-services") version "4.4.4" apply false
 }
