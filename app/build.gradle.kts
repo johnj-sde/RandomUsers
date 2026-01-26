8f15961272ae37710bfe44bf6abfbbd923327a32
NonAdminWork <nonadminwork@Johns-MacBook-Pro.local>
2026-01-25 18:39:16 -0600

    add Nav3 dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,7 +80,8 @@ dependencies {
 
     implementation(libs.bundles.retrofit)
 
-    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.navigation3.runtime)
+    implementation(libs.androidx.navigation3.ui)
 
     implementation(libs.kotlinx.serialization.json)
 
