ace8412b36342aa6ad5c55b969d5107ed027652e
NonAdminWork <nonadminwork@Johns-MacBook-Pro.local>
2026-02-09 21:39:29 -0600

    deleting unused classes from old navigation framework

--- app/src/main/java/com/developer/randomusers/MainActivity.kt
+++ app/src/main/java/com/developer/randomusers/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.developer.randomusers
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
-import com.developer.randomusers.ui.navigation.NavHostContainer
 import com.developer.randomusers.ui.navigation.NavRoot
 import com.developer.randomusers.ui.theme.RandomUsersTheme
 
