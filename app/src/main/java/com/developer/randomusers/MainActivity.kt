8490cec5e28d616c3a5098f9989746a79ee7b56f
NonAdminWork <nonadminwork@Johns-MacBook-Pro.local>
2026-02-12 22:16:01 -0600

    adding intent as stae to trigger UI updates and navigation when launched by deeplink

--- app/src/main/java/com/developer/randomusers/MainActivity.kt
+++ app/src/main/java/com/developer/randomusers/MainActivity.kt
@@ -1,22 +1,33 @@
 package com.developer.randomusers
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.mutableStateOf
 import com.developer.randomusers.ui.navigation.NavRoot
 import com.developer.randomusers.ui.theme.RandomUsersTheme
 
 class MainActivity : ComponentActivity() {
 
+    private var intentUri = mutableStateOf<Uri?>(null)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        intentUri.value = intent.data
         enableEdgeToEdge()
         setContent {
             RandomUsersTheme {
-                NavRoot(deeplink = intent.data)
+                NavRoot(deeplink = intentUri.value)
             }
         }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        intentUri.value = intent.data
     }
 
 }
